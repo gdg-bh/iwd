3affdaf32cb7cc13d1f38e944c510c4602b18c52
Ana Soares <anne.cr.soares@gmail.com>
2017-02-23 18:53:01 -0300

    Alterações de conteúdo

--- js/main.js
+++ js/main.js
@@ -1,96 +1,27 @@
-(function () {
-  'use strict';
+function initialize() {
+  var mapCanvas = document.getElementById('map-canvas');
+  var mapOptions = {
+    center: new google.maps.LatLng(-19.920430, -43.921115),
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scrollwheel: false
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions);
 
-  var conf = {};
+  var WTMs = [
+      ['Google Belo Horizonte', -19.920430, -43.921115],
+  ];
 
-  // Init functions, called on DOMContentLoaded event
-  conf.init = function () {
-    conf.map.init($('#map-canvas'));
-    conf.menu.init();
-  };
+  var marker, i;
 
-  /***
-    Google Maps implementation
-  ***/
-  conf.map = {
-    marker: 'img/marker-default.png'
-  };
+  for( i = 0; i < WTMs.length; i++ ) {
+    var position = new google.maps.LatLng(WTMs[i][1], WTMs[i][2]);
+        marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: WTMs[i][0]
+      });
 
-  // Google Maps configs
-  conf.map.init = function ($element) {
-    conf.map.element = $element;
-
-    conf.map.geocoder = new google.maps.Geocoder();
-
-    conf.map.latlng = new google.maps.LatLng(0, 0);
-
-    conf.map.options = {
-      zoom: 16,
-      center: conf.map.latlng,
-      scrollwheel: false,
-      streetViewControl: true,
-      labels: true,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-
-    conf.map.canvas = new google.maps.Map(conf.map.element.get(0), conf.map.options);
-    conf.map.canvas.setCenter(conf.map.latlng);
-
-    conf.map.createMarker();
-  };
-
-  conf.map.createMarker = function () {
-
-    conf.map.address = conf.map.element.attr('data-address');
-
-    conf.map.geocoder.geocode({ 'address': conf.map.address}, function (results, status) {
-
-      if (status === google.maps.GeocoderStatus.OK) {
-
-        conf.map.canvas.setCenter(results[0].geometry.location);
-
-        new google.maps.Marker({
-          map: conf.map.canvas,
-          position: results[0].geometry.location,
-          icon: conf.map.marker
-        });
-      } else {
-        if (window.console) {
-          console.log('Google Maps was not loaded: ', status);
-        }
-      }
-    });
-  };
-
-  /***
-    Create animated scroll for menu links
-  ***/
-  conf.menu = {
-    itemsSelector: '.nav-link[href^="#"]',
-    animationSpeed: 400
-  };
-
-  conf.menu.init = function () {
-    conf.menu.menuItems = $(conf.menu.itemsSelector);
-    conf.menu.document = $('html, body');
-
-    conf.menu.menuItems.on('click.animateScroll', function (event) {
-        event.preventDefault();
-
-        conf.menu.animateTo(event.target);
-    });
-  };
-
-  conf.menu.animateTo = function (link) {
-
-    var $link = $(link),
-        href = $link.attr('href'),
-        offSetTop = $(href).offset().top;
-
-    conf.menu.document.finish().animate({scrollTop : offSetTop}, conf.menu.animationSpeed, function () {
-        location.hash = href;
-    });
-  };
-
-  conf.init();
-}());
+}
+google.maps.event.addDomListener(window, 'load', initialize);
